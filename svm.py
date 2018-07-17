d199d662dce559564690add5751ac74f568ebba9
Clay <chapmacl@outlook.com>
2018-07-17 15:49:40 -0400

    Updated some lines, added a few things here and there

--- svm.py
+++ svm.py
@@ -2,6 +2,7 @@
 from sklearn import datasets
 from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import (CountVectorizer, TfidfTransformer)
@@ -10,10 +11,11 @@ import matplotlib.pyplot as plt
 import itertools
 import numpy as np 
 import csv
+import time
  
 features = []
 labels = []
-
+start = time.time()
 print('Loading data...')
 #load the data from the csv file into a workable array
 with open('predict.csv', 'r') as f:
@@ -42,7 +44,7 @@ for row in X:
 
 
 # dividing X, y into train and test data
-X_train, X_test, y_train, y_test = train_test_split(testX, y, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(testX, y, test_size=0.30, random_state = 0)
  
 # training a linear SVM classifier
 print('Training SVM...')
@@ -52,10 +54,13 @@ svm_predictions = svm_model_linear.predict(X_test)
 # model accuracy for X_test  
 accuracy = svm_model_linear.score(X_test, y_test)
 print('SVM Model is: ' + str(accuracy) + ' accurate')
+stop = time.time()
+print('This model took ' + str(stop-start) + ' seconds to complete')
  
 # creating a confusion matrix
 cm = confusion_matrix(y_test, svm_predictions)
-print(cm)
+print(classification_report(y_test, svm_predictions))
+#print(cm)
 
 #For saving the model to Disk
 '''
